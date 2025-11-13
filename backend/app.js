dc319056740b40f5f0e904b42a0b190670b07752
Juan Puerta <juan.pablo.puerta@correounivalle.edu.co>
2025-11-12 19:26:50 -0500

    major cors update

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// serve static UI from public (create public/index.html)
+app.use(cors());
+
 app.use(express.static('public'));
 
-// mount your API routes at root so register becomes /register
 const routes = require('./src/routes/index');
 app.use('/', routes);
 
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
